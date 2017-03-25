4b8c7ce99518ef8c40877252bd828a7190109159
JUNDA YIN <yinjunda@msu.edu>
2017-03-24 20:45:48 -0400

    Set up the camera

--- Project 1/Project 1/ChildView.cpp
+++ Project 1/Project 1/ChildView.cpp
@@ -65,7 +65,61 @@ void CChildView::CreateSceneGraph()
 
 void CChildView::OnGLDraw(CDC* pDC)
 {
-	//glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-	//glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//
+	// Set up the camera
+	//
+
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+
+	// Determine the screen size so we can determine the aspect ratio
+	int width, height;
+	GetSize(width, height);
+	GLdouble aspectratio = GLdouble(width) / GLdouble(height);
+
+	// Set the camera parameters
+	gluPerspective(25.,         // Vertical FOV degrees.
+		aspectratio, // The aspect ratio.
+		10.,         // Near clipping 40/130
+		200.);       // Far clipping
+
+					 // Set the camera location
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+
+	gluLookAt(20., 10., 50.,    // eye x,y,z
+		0., 0., 0.,       // center x,y,z
+		0., 1., 0.);      // Up direction
+
+						  //
+						  // Some standard parameters
+						  //
+
+						  // Enable depth test
+	glEnable(GL_DEPTH_TEST);
+
+	// Cull backfacing polygons
+	glCullFace(GL_BACK);
+	glEnable(GL_CULL_FACE);
+
+	// Draw a coordinate axis
+	glColor3d(0., 1., 1.);
+
+	glBegin(GL_LINES);
+	glVertex3d(0., 0., 0.);
+	glVertex3d(12., 0., 0.);
+	glVertex3d(0., 0., 0.);
+	glVertex3d(0., 12., 0.);
+	glVertex3d(0., 0., 0.);
+	glVertex3d(0., 0., 12.);
+	glEnd();
+
+
+	// 
+	// INSERT DRAWING CODE HERE
+	//
 	m_scenegraph->Render();
 }
